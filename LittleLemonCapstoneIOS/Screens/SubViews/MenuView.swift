cc5d56a005ace2d4d77d185526099a2211df768f
kedrakk <keaindrak@gmail.com>
2023-09-04 20:49:31 +0630

    image resize

--- LittleLemonCapstoneIOS/Screens/SubViews/MenuView.swift
+++ LittleLemonCapstoneIOS/Screens/SubViews/MenuView.swift
@@ -14,8 +14,21 @@ struct MenuView: View {
                 ForEach(dishes) { dishItem in
                     HStack{
                         Text("\(dishItem.title ?? "") with \(dishItem.price ?? "") $")
-                        AsyncImage(url: URL(string: "\(dishItem.image ?? "")"))
-                            .frame(width: 200, height: 200)
+                        Spacer()
+                        //https://talksport.com/wp-content/uploads/sites/5/2020/09/image-a4262f158a.jpg?w=620
+                        //"\(dishItem.image ?? "")"
+                        AsyncImage(
+                            url:URL(string: "\(String(describing: dishItem.image))"),
+                                        content: { image in
+                                            image.resizable()
+                                                 .aspectRatio(contentMode: .fit)
+                                                 .frame(maxWidth: 100, maxHeight: 100)
+                                        },
+                                        placeholder: {
+                                            //ProgressView()
+                                            Rectangle().frame(width:  100, height: 70).foregroundColor(.secondaryBlack)
+                                        }
+                                    )
 
                     }
                 }
