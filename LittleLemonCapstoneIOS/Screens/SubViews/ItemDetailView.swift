5f6b55388321ee69e45d3dc6ec68d94162f2b51a
kedrakk <keaindrak@gmail.com>
2023-09-10 16:04:51 +0630

    dish item detail

--- LittleLemonCapstoneIOS/Screens/SubViews/ItemDetailView.swift
+++ LittleLemonCapstoneIOS/Screens/SubViews/ItemDetailView.swift
@@ -15,7 +15,44 @@ struct ItemDetailView: View {
     }
     
     var body: some View {
-        Text("\(dish.title ?? "Null Title")")
+        ScrollView{
+            VStack{
+                AsyncImage(
+                    url:URL(string: "\(dish.image ?? "")"),
+                    content: { image in
+                        image.resizable()
+                            .frame(maxWidth: .infinity)
+                            .aspectRatio(contentMode: .fit)
+                    },
+                    placeholder: {
+                        Rectangle()
+                            .frame(maxWidth: .infinity, minHeight:300, maxHeight:300)
+                            .foregroundColor(.secondaryBlack)
+                    }
+                )
+                Text("$ \(dish.price ?? "")")
+                    .fontWeight(.heavy)
+                    .font(.karlaLeadText)
+                    .foregroundColor(.primaryBlue)
+                    .frame(maxWidth: .infinity,alignment: .trailing)
+                    .padding(.horizontal,10)
+                    .padding(.vertical,5)
+                Text("\(dish.desc ?? "")")
+                    .lineLimit(2)
+                    .fontWeight(.light)
+                    .font(.karlaParagraph)
+                    .foregroundColor(.primaryBlue)
+                    .padding(.horizontal,10)
+                    .padding(.vertical,5)
+            }.navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        Text("\(dish.title ?? "")")
+                            .font(.karlaLeadText)
+                            .padding(EdgeInsets(top: 2, leading: 0, bottom: 2, trailing: 0))
+                    }
+                }
+        }
     }
 }
 
