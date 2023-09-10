582bb0d1c16f69f04006a2a815dd816ef13fdfe6
kedrakk <keaindrak@gmail.com>
2023-09-10 10:45:55 +0630

    filter by category

--- LittleLemonCapstoneIOS/Model/MenuCategory.swift
+++ LittleLemonCapstoneIOS/Model/MenuCategory.swift
@@ -7,38 +7,5 @@
 
 import Foundation
 
-class MenuCategory:Identifiable,ObservableObject {
-    let id: UUID
-    @Published var selected:Bool
-    let title:String
-    
-    init(selected: Bool, title: String) {
-        self.id = UUID()
-        self.selected = selected
-        self.title = title
-    }
-}
-
-class MenuCategoryList:ObservableObject{
-    @Published var allMenuCategoryList:[MenuCategory]
-    
-    init(allMenuCategoryList:[MenuCategory]) {
-        self.allMenuCategoryList = allMenuCategoryList
-    }
-}
-
-class MenuCategoryListRepo:ObservableObject{
-    @Published var menuCategoryList:MenuCategoryList
-    
-    init() {
-        menuCategoryList = MenuCategoryList(
-            allMenuCategoryList:[
-                            MenuCategory(selected: false, title: "Starters"),
-                            MenuCategory(selected: false, title: "Mains"),
-                            MenuCategory(selected: false, title: "Desserts"),
-                            MenuCategory(selected: false, title: "Drinks")
-                        ]
-        )
-    }
-}
+let allMenuCategory = ["Starters","Mains","Desserts","Drinks"]
 
--- LittleLemonCapstoneIOS/Screens/SubViews/MenuCategoryListView.swift
+++ LittleLemonCapstoneIOS/Screens/SubViews/MenuCategoryListView.swift
@@ -7,44 +7,21 @@
 
 import SwiftUI
 
-struct MenuCategoryListView: View {
-    
-    @StateObject var allMenuCategoryList = MenuCategoryListRepo()
-    
-    var body: some View {
-        VStack(alignment: .leading){
-            Text("ORDER FOR DELIVERY!").bold()
-            ScrollView(.horizontal) {
-                HStack{
-                    ForEach(allMenuCategoryList.menuCategoryList.allMenuCategoryList.indices,id: \.self) { index in
-                        Button {
-                            allMenuCategoryList.menuCategoryList.allMenuCategoryList[index].selected.toggle()
-                        } label: {
-                            MenuCategoryListItem().environmentObject(allMenuCategoryList.menuCategoryList.allMenuCategoryList[index])
-                        }
-
-                    }
-                }
-            }.padding(.vertical,10)
-            Divider()
-        }.padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
-    }
-}
-
 struct MenuCategoryListItem:View {
-    @EnvironmentObject var menuCategory:MenuCategory
+    let title:String
+    let isSelected:Bool
+    
+    init(title: String,isSelected:Bool) {
+        self.title = title
+        self.isSelected = isSelected
+    }
+    
     var body: some View{
-        Text(menuCategory.title)
+        Text(title)
             .bold()
             .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
-            .background(menuCategory.selected ? Color.primaryBlue : Color.secondaryWhite)
+            .background(isSelected ? Color.primaryBlue : Color.secondaryWhite)
             .cornerRadius(30)
-            .foregroundColor(menuCategory.selected ? Color.secondaryWhite : Color.primaryBlue)
-    }
-}
-
-struct MenuCategoryListView_Previews: PreviewProvider {
-    static var previews: some View {
-        MenuCategoryListView()
+            .foregroundColor(isSelected ? Color.secondaryWhite : Color.primaryBlue)
     }
 }
