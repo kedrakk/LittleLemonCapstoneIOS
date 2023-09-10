929bce571f6f3f08d051f02e13c1be082cb659d7
kedrakk <keaindrak@gmail.com>
2023-09-10 16:24:54 +0630

    onboarding

--- LittleLemonCapstoneIOS/Screens/OnboardingView.swift
+++ LittleLemonCapstoneIOS/Screens/OnboardingView.swift
@@ -19,47 +19,56 @@ struct OnboardingView: View {
     
     var body: some View {
         NavigationStack {
-            VStack{
-                AppTextView(label: "First Name", value: $firstName)
-                AppTextView(label: "Last Name", value: $lastName)
-                AppTextView(label: "Email", value: $email)
-                AppButtonView(title: "Register") {
-                    if firstName.isEmpty || lastName.isEmpty || email.isEmpty
-                    {
-                        showAlert=true
-                        message = "All fields must be filled"
+            ScrollView{
+                VStack{
+                    AppHeroView(bottomView: nil).padding(.vertical,5)
+                    AppTextView(label: "First Name", value: $firstName)
+                    AppTextView(label: "Last Name", value: $lastName)
+                    AppTextView(label: "Email", value: $email)
+                    AppButtonView(title: "Register") {
+                        if firstName.isEmpty || lastName.isEmpty || email.isEmpty
+                        {
+                            showAlert=true
+                            message = "All fields must be filled"
+                        }
+                        else
+                        {
+                            UserDefaults.standard.set(
+                                firstName, forKey: kFirstName
+                            )
+                            UserDefaults.standard.set(
+                                lastName, forKey: kLastName
+                            )
+                            UserDefaults.standard.set(
+                                email, forKey: kEmail
+                            )
+                            UserDefaults.standard.set(
+                                true, forKey: kIsLoggedIn
+                            )
+                            isLoggedIn=true
+                        }
                     }
-                    else
-                    {
-                        UserDefaults.standard.set(
-                            firstName, forKey: kFirstName
-                        )
-                        UserDefaults.standard.set(
-                            lastName, forKey: kLastName
-                        )
-                        UserDefaults.standard.set(
-                            email, forKey: kEmail
-                        )
-                        UserDefaults.standard.set(
-                            true, forKey: kIsLoggedIn
-                        )
-                        isLoggedIn=true
+                    .alert(isPresented: $showAlert) {
+                        Alert(title: Text(message))
                     }
-                }
-                .alert(isPresented: $showAlert) {
-                    Alert(title: Text(message))
-                }
 
-            }
-            .navigationTitle("Little Lemon")
-            .navigationDestination(isPresented: $isLoggedIn) {
-                HomeView().environment(\.managedObjectContext, self.viewContext)
-            }
-            .onAppear(){
-                Menu().getMenuData(viewContext)
+                }
+                //.navigationTitle("Little Lemon")
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationDestination(isPresented: $isLoggedIn) {
+                    HomeView().environment(\.managedObjectContext, self.viewContext)
+                }
+                .toolbar(content: {
+                    ToolbarItem(placement: .principal) {
+                        Image("Logo")
+                    }
+                })
+                .onAppear(){
+                    Menu().getMenuData(viewContext)
+                }
+                .padding(.horizontal,-1)
             }
         }
-        
         
     }
 }
